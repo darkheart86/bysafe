73d1c41fbb9d27b4c64652155c37209280f08afe
Alexandre Monpean <a.monpean.dev@gmail.com>
2023-09-23 22:36:03 -0300

    Formatado o código

--- bysafe.c
+++ bysafe.c
@@ -12,76 +12,82 @@ int led2 = 12;
 int led3 = 14;
 int bzz = 4;
 
-
 ESP8266WebServer server(80);
 
 void actLED();
 void piscaLED(int);
-void handleLED() {};
-void handleRoot() {
+void handleLED(){};
+void handleRoot()
+{
 
-server.send(200, "text/html", "<p><html> <head> <title>ESP8266 LED Control</title> </head> <body> <center><font size='40'> <a href='/?led=1'>Led 1 </a><br> <a href='/?led=2'> Led 2 </a><br> <a href='/?led=3'> Led 3 </a><br></font> </center> </body> </html></p>");
-actLED();
+    server.send(200, "text/html", "<p><html> <head> <title>ESP8266 LED Control</title> </head> <body> <center><font size='40'> <a href='/?led=1'>Led 1 </a><br> <a href='/?led=2'> Led 2 </a><br> <a href='/?led=3'> Led 3 </a><br></font> </center> </body> </html></p>");
+    actLED();
 }
 
+void setup()
+{
 
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(led3, OUTPUT);
+    pinMode(bzz, OUTPUT);
 
-void setup() {
+    delay(1000);
 
-pinMode (led1, OUTPUT);
-pinMode (led2, OUTPUT);
-pinMode (led3, OUTPUT);
-pinMode(bzz,OUTPUT);
+    Serial.begin(9600);
+    Serial.println();
+    Serial.print("Configuring access point...");
+    WiFi.softAP(ssid, password);
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
 
-delay(1000);
-
-Serial.begin(9600);
-Serial.println();
-Serial.print("Configuring access point...");
-WiFi.softAP(ssid, password);
-IPAddress myIP = WiFi.softAPIP();
-Serial.print("AP IP address: ");
-Serial.println(myIP);
-
-server.on("/", handleRoot);
-server.begin();
-Serial.println("HTTP server started");
-
+    server.on("/", handleRoot);
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
-void loop() {
-server.handleClient();
+void loop()
+{
+    server.handleClient();
 }
 
+void piscaLED(int led)
+{
+    int i;
 
+    for (i = 0; i < 20; i++)
+    {
+        delay(500);
+        if (!digitalRead(led))
+        {
+            Serial.println(digitalRead(led));
+            digitalWrite(led, HIGH);
+            digitalWrite(bzz, HIGH);
+        }
+        else
+        {
+            Serial.println(digitalRead(led));
+            digitalWrite(led, LOW);
+            digitalWrite(bzz, LOW);
+        }
+    }
+}
 
-void piscaLED(int led) {
-int i;
+void actLED()
+{
 
-for(i=0;i<20;i++){
-delay(500);
-if (!digitalRead(led)) {
-Serial.println(digitalRead(led));
-digitalWrite(led,HIGH);
-digitalWrite(bzz,HIGH);
-}else{
-Serial.println(digitalRead(led));
-digitalWrite(led,LOW);
-digitalWrite(bzz,LOW);
-}
-}
-}
-
-void actLED(){
-
-String state=server.arg("led");
-if (state=="1") {
-piscaLED(led1);
-}
-if (state=="2") {
-piscaLED(led2);
-}
-if (state=="3") {
-piscaLED(led3);
-}
+    String state = server.arg("led");
+    if (state == "1")
+    {
+        piscaLED(led1);
+    }
+    if (state == "2")
+    {
+        piscaLED(led2);
+    }
+    if (state == "3")
+    {
+        piscaLED(led3);
+    }
 }
